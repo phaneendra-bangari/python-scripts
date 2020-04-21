a7132cd1eb4539afdc488b6d82c2de707ea5d9e7
Phaneendra Bangari ATG <phanee.bangari@gmail.com>
2020-04-21 07:57:29 +0000

    Added the file contributorDetails.py

---
+++ contributorDetails.py
@@ -0,0 +1,7 @@
+'''
+Fill this sheet if you want to contribute this repository. Use the below as template.
+
+Name: Phaneendra Bangari ATG
+Age: 26
+Source Location: DELL Office Laptop
+Email: phanee.bangari@gmail.com
