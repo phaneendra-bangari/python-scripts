f5fd6751afce6a6e0373708e23eb898a5520b25e
Phaneendra Bangari ATG <phanee.bangari@gmail.com>
2020-04-21 07:56:54 +0000

    Added the file ownerInformation.py

---
+++ ownerInformation.py
@@ -0,0 +1,8 @@
+Hello!
+My name is Phaneendra!
+As Python is not yet installed on my Instance yet. I am using this file to test the GIT Versions and learning the same.
+
+Name: Phaneendra
+Occupation: Senior Devops Engineer
+Email: phanee.bangari@gmail.com
+Location: Hyderabad
