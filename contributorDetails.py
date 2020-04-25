7075bc6c439f41b3f175bee55677e3761fbbc4be
Phaneendra Bangari <37768819+phaneendra-bangari@users.noreply.github.com>
2020-04-25 08:59:52 +0530

    Adding my user from GITHUB GUI

--- contributorDetails.py
+++ contributorDetails.py
@@ -6,3 +6,11 @@ Name: Phaneendra Bangari ATG
 Age: 26
 Source Location: DELL Office Laptop
 Email: phanee.bangari@gmail.com
+
+'''
+Adding from GITHUB GUI
+'''
+Name: Phaneendra Bangari GITHUB
+Age: 26
+Source Location: DELL Office Laptop - GITHUB
+Email: phaneendra.bangari@gmail.com
