f8d0c2ac9fcb8d37305a6fc181d0fa5e77461159
Phaneendra Bangari ATG <phanee.bangari@gmail.com>
2020-04-28 11:34:59 +0000

    Adding more information to UnderstandingDiff.py

--- UnderstandingDiff.py
+++ UnderstandingDiff.py
@@ -1,3 +1,29 @@
 Hello I am Line1.
 
+Hello I am Line 2. I will be in staging area.
+
+Hello I am Line 3. I will be in untracked area.
+
+
+Additional Learning information
+
+  287  vi UnderstandingDiff.py
+  288  git commit -am "Created a file UnderstandingDiff.py"
+  289  git add UnderstandingDiff.py
+  290  ls -ltr
+  291  git commit -m "Created a file UnderstandingDiff.py"
+  292  ls -ltr
+  293  vi UnderstandingDiff.py
+  294  git add UnderstandingDiff.py
+  295  git status
+  296  vi UnderstandingDiff.py
+  297  git status
+  298  git diff
+  299  git diff -- HEAD UnderstandingDiff.py
+  300  git log --oneline
+  301  git diff 828a994 8475db7
+  302  ls -ltr
+  303  git status
+  304  git diff master origin/master
+  305  history
 
