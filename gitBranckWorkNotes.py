14cf97ac74365afb9a066e59b6be17df0c204609
Phaneendra Bangari ATG <phanee.bangari@gmail.com>
2020-04-30 06:33:02 +0000

    Added more content to gitBranckWorkNotes.py from temporarynotes branch

--- gitBranckWorkNotes.py
+++ gitBranckWorkNotes.py
@@ -2,4 +2,6 @@ Hi I am a file which is creted with Worknotes as the branch.
 
 Adding more information to test merging concepts.
 
+Testing the modification of same file with different merges.
 
+This line is added using, temporarynotes branch.
