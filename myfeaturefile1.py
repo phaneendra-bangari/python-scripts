775d53e35f72dd1b7968ee8733a97dc8a7b5aa8e
Phaneendra Bangari ATG <phanee.bangari@gmail.com>
2020-05-01 10:00:52 +0000

    Finished changes on the file myfeaturefile1.py after the stashing. Commiting after the stash is removed

--- myfeaturefile1.py
+++ myfeaturefile1.py
@@ -3,5 +3,7 @@ Helloo! I am a new file created in myfeature branch.
 Adding a line from master branch in this file.
 Adding a line from myfeature branch to identify conflicts.
 
-Adding more content to this file to understand rebase. Adding this content from master branch
+Adding more content to this file to understand rebase. Adding this content from master branch.
+
+Adding more information temporarily to move to stashing. This is not a new file.
 
