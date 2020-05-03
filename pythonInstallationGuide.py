0585fa0efd24eef6fd1636d5ea9c38c37fc5c77a
Phaneendra Bangari ATG <phanee.bangari@gmail.com>
2020-05-03 11:34:17 +0000

    Removed the un-necessary files and restructured them by creating directories. This will be the initial version of the Scripts

---
+++ pythonInstallationGuide.py
@@ -0,0 +1,13 @@
+Steps to install Python 3.8.0 in Redhat Linux 8.
+
+Do not uninstall any in build Python Packages on your kernel. This may damage some of your system properties.
+
+You can use the interpreter python3 for your script executions.
+
+sudo yum install python38*
+python3 --version
+pip3 --version
+
+Note: This is not a script yet. Its just an information document, do not mis guide yourself by seeing .py at the end of the file name.
+
+Thankyou.
