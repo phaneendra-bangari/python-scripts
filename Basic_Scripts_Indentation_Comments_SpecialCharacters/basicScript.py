df1ce44c9b8ee6650676ccc039fb2690b2baf3c4
Phaneendra Bangari ATG <phanee.bangari@gmail.com>
2020-05-03 11:58:52 +0000

    Completed some basics of Scripts. Basic_Scripts_Indentation_Comments_SpecialCharacters

---
+++ Basic_Scripts_Indentation_Comments_SpecialCharacters/basicScript.py
@@ -0,0 +1,14 @@
+#This is owned by Phaneendra Bangari
+
+#Single line comment
+'''
+This is multiline comments.
+Another line to test multiline comments.
+'''
+
+print("If you get this output it means you learnt how to use comments.")
+if 3 > 1:
+	print("If you get this line, it means you learned indentation.")
+	print("Congratulations!!")
+print('\tIf you get this line, it means you learned special characters too.\nThere is a lot more to learn buddie\'s!!')
+
