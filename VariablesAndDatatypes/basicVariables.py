bed22e725a49c6d8e43bc41c3b8c13d30d357e53
Phaneendra Bangari ATG <phanee.bangari@gmail.com>
2020-05-04 02:59:23 +0000

    Adding VariablesAndDatatypes Directory and basicVariables.py file in it.

---
+++ VariablesAndDatatypes/basicVariables.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+#Owner: Phaneendra Bangari
+#Contact: 09550328609
+#Email: phaneendra.bangari@gmail.com
+
+'''
+I am learning and writing Python Scripting to make Automation.
+Purposedly I am using GITLAB to maintain the versions of files.
+'''
+
+#This Python Script is to understand Variables and print them.
+
+#Declaring variables
+I_AM_VARIABLE_1=30
+I_AM_VARIABLE_2=50.5
+
+#Printing declared variables
+print(I_AM_VARIABLE_1)
+print(I_AM_VARIABLE_2)
+
+#Printing type of declared variables
+print(type(I_AM_VARIABLE_1))
+print(type(I_AM_VARIABLE_2))
+
+#Deleting the variables from the RAM memory as we no longer need them.
+del I_AM_VARIABLE_1
+del I_AM_VARIABLE_2
+
